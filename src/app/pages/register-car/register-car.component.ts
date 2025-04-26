4a14d901beea2e069d0eabfbe6f5a4c19a4f12db
Paulo Vitor Rosendo <paulovitor.rsd@gmail.com>
2025-04-26 16:49:40 -0300

    feat: last update before refactor

--- src/app/pages/register-car/register-car.component.ts
+++ src/app/pages/register-car/register-car.component.ts
@@ -10,18 +10,29 @@ import { Brand } from '../../_models/brand';
   styleUrl: './register-car.component.css'
 })
 export class RegisterCarComponent implements OnInit{
+  
   @Input() brand: Brand = {id: 0, brandId: 0, brandName: ''};
   
   brands: any = [];
 
-  carForm = new FormGroup({
-    brand: new FormControl('', Validators.required),
-  });
-    
-  constructor(private carService: CarService) { }
+  carForm: any = new FormGroup({});
+
+  constructor(private fb: FormBuilder, private carService: CarService) { }
 
   ngOnInit(): void {
     this.loadBrands();
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.carForm = this.fb.group({
+      brand: ['', [Validators.required]],
+      model: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(100)]],
+      year: [0, [Validators.required, Validators.min(1800), Validators.max(2030)]],
+      price: [0, [Validators.required, Validators.min(0), Validators.max(1000000000)]],
+      displacement: ["", [Validators.required]],
+      carType: ["", [Validators.required]]
+    })
   }
 
   loadBrands(): void {
@@ -36,9 +47,9 @@ export class RegisterCarComponent implements OnInit{
   }
 
   SubmitForm(): void {
-    if (this.carForm.valid) {
-      const selectedBrand = this.carForm.get('brand')?.value;
-      console.log('Selected name:', selectedBrand);
+    if(this.carForm.valid){
+      this.carService.addCar(this.carForm.value).subscribe();
+      this.carForm.reset();
     }
   }
 }
