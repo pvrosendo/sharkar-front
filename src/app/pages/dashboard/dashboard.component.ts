b19431d09556994f73c7b0a1aaa5f6faba2c227d
Paulo Vitor Rosendo <paulovitor.rsd@gmail.com>
2025-05-21 19:29:01 -0300

    feat: add recent cars list on dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CarService } from '../../_services/car.service';
+import { Car } from '../../_models/car';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,14 +10,32 @@ import { Router } from '@angular/router';
   standalone: false
 })
 export class DashboardComponent implements OnInit {
-  recentCars: any[] = [];
+  recentCars: Car[] = [];
   priceAlerts: any[] = [];
   isLoading = true;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private carService: CarService
+  ) {}
 
   ngOnInit() {
-    this.isLoading = false;
+    this.loadRecentCars();
+  }
+
+  loadRecentCars() {
+    this.isLoading = true;
+    this.carService.getAllCars().subscribe({
+      next: (data: any) => {
+        
+        this.recentCars = data.slice(0, 4);
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Error loading recent cars:', error);
+        this.isLoading = false;
+      }
+    });
   }
 
   navigateToRegister() {
