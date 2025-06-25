b4b76bee558cb9ecbdcebd0186cb98ddc90f0049
Paulo Vitor Rosendo <paulovitor.rsd@gmail.com>
2025-06-24 21:36:32 -0300

    refactor: update texts and name of project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = { 
-  apiUrl: 'http://localhost:8080/dream-car', 
-  apiUrlFipe: 'http://localhost:8080/dream-car/fipe',
-  apiUrlAuth: 'http://localhost:8080/dream-car/auth'
+  apiUrl: 'http://localhost:8080/sharkar', 
+  apiUrlFipe: 'http://localhost:8080/sharkar/fipe',
+  apiUrlAuth: 'http://localhost:8080/sharkar/auth'
 };
