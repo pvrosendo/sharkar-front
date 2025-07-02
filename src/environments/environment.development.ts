5968f5393bbb03c609969de8e572e540b77e8843
Paulo Vitor Rosendo <paulovitor.rsd@gmail.com>
2025-07-02 20:21:21 -0300

    ?

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = { 
-  apiUrl: 'https://3.16.128.67:8080/sharkar', 
-  apiUrlFipe: 'https://3.16.128.67:8080/sharkar/fipe',
-  apiUrlAuth: 'https://3.16.128.67:8080/sharkar/auth'
+  apiUrl: 'http://3.16.128.67:8080/sharkar', 
+  apiUrlFipe: 'http://3.16.128.67:8080/sharkar/fipe',
+  apiUrlAuth: 'http://3.16.128.67:8080/sharkar/auth'
 };
